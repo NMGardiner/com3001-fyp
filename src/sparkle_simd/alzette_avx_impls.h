fd3f20ce6efc9ed3cc0e4c7c19c6059ba877868b
Nathan Gardiner <nathangardiner@protonmail.com>
2023-03-11 16:58:51 +0000

    Experiment with unrolling the linear layer for Sparkle
    I had the idea to try keeping the state in the SIMD registers when
    running the linear layer, with the idea that avoiding the extra
    load/stores could be benficial for performance. This proves to be
    counterproductive, as accessing individual elements in the SIMD
    registers through a union can actually force the data to be flushed
    to memory first. This causes the performance to be much worse than
    reference (~30% slower), and so is not worth it.

--- src/sparkle_simd/alzette_avx_impls.h
+++ src/sparkle_simd/alzette_avx_impls.h
@@ -411,3 +411,226 @@ __inline void alzette_avx_05(uint32_t* state, int brans) {
         }
     }
 }
+
+
+// Define these as macros to allow changing to match the indices when doing a shuffled
+// load with gather.
+#define INDEX_0 0
+#define INDEX_1 8
+#define INDEX_2 1
+#define INDEX_3 9
+#define INDEX_4 4
+#define INDEX_5 12
+#define INDEX_6 5
+#define INDEX_7 13
+#define INDEX_8 2
+#define INDEX_9 10
+#define INDEX_10 3
+#define INDEX_11 11
+#define INDEX_12 6
+#define INDEX_13 14
+#define INDEX_14 7
+#define INDEX_15 15
+
+// An unrolled version of the linear layer for 4, 6, or 8 branches.
+// Uses the macros for indices, so that it works no matter how the state is shuffled.
+__inline void linear_layer_unrolled(uint32_t* state, int brans, uint32_t tmpx, uint32_t tmpy, uint32_t x0, uint32_t y0) {
+    tmpx = x0 = state[INDEX_0];
+    tmpy = y0 = state[INDEX_1];
+
+    if (brans >= 4) {
+        tmpx ^= state[INDEX_2];
+        tmpy ^= state[INDEX_3];
+    }
+
+    if (brans >= 6) {
+        tmpx ^= state[INDEX_4];
+        tmpy ^= state[INDEX_5];
+    }
+
+    if (brans == 8) {
+        tmpx ^= state[INDEX_6];
+        tmpy ^= state[INDEX_7];
+    }
+
+    tmpx = ELL(tmpx);
+    tmpy = ELL(tmpy);
+
+    if (brans == 4) {
+        // j = 2
+        state[INDEX_0] = state[INDEX_6] ^ state[INDEX_2] ^ tmpy;
+        state[INDEX_6] = state[INDEX_2];
+        state[INDEX_1] = state[INDEX_7] ^ state[INDEX_3] ^ tmpx;
+        state[INDEX_7] = state[INDEX_3];
+
+        state[INDEX_2] = state[INDEX_4] ^ x0 ^ tmpy;
+        state[INDEX_4] = x0;
+        state[INDEX_3] = state[INDEX_5] ^ y0 ^ tmpx;
+        state[INDEX_5] = y0;
+    } else if (brans == 6) {
+        // j = 2
+        state[INDEX_0] = state[INDEX_8] ^ state[INDEX_2] ^ tmpy;
+        state[INDEX_8] = state[INDEX_2];
+        state[INDEX_1] = state[INDEX_9] ^ state[INDEX_3] ^ tmpx;
+        state[INDEX_9] = state[INDEX_3];
+
+        // j = 4
+        state[INDEX_2] = state[INDEX_10] ^ state[INDEX_4] ^ tmpy;
+        state[INDEX_10] = state[INDEX_4];
+        state[INDEX_3] = state[INDEX_11] ^ state[INDEX_5] ^ tmpx;
+        state[INDEX_11] = state[INDEX_5];
+
+        state[INDEX_4] = state[INDEX_6] ^ x0 ^ tmpy;
+        state[INDEX_6] = x0;
+        state[INDEX_5] = state[INDEX_7] ^ y0 ^ tmpx;
+        state[INDEX_7] = y0;
+    } else if (brans == 8) {
+        // j = 2
+        state[INDEX_0] = state[INDEX_10] ^ state[INDEX_2] ^ tmpy;
+        state[INDEX_10] = state[INDEX_2];
+        state[INDEX_1] = state[INDEX_11] ^ state[INDEX_3] ^ tmpx;
+        state[INDEX_11] = state[INDEX_3];
+
+        // j = 4
+        state[INDEX_2] = state[INDEX_12] ^ state[INDEX_4] ^ tmpy;
+        state[INDEX_12] = state[INDEX_4];
+        state[INDEX_3] = state[INDEX_13] ^ state[INDEX_5] ^ tmpx;
+        state[INDEX_13] = state[INDEX_5];
+
+        // j = 6
+        state[INDEX_4] = state[INDEX_14] ^ state[INDEX_6] ^ tmpy;
+        state[INDEX_14] = state[INDEX_6];
+        state[INDEX_5] = state[INDEX_15] ^ state[INDEX_7] ^ tmpx;
+        state[INDEX_15] = state[INDEX_7];
+
+        state[INDEX_6] = state[INDEX_8] ^ x0 ^ tmpy;
+        state[INDEX_8] = x0;
+        state[INDEX_7] = state[INDEX_9] ^ y0 ^ tmpx;
+        state[INDEX_9] = y0;
+    }
+}
+
+
+// Original version of the linear layer, without loop unrolling or modified indices.
+__inline void linear_layer_rolled(uint32_t* state, int brans, uint32_t tmpx, uint32_t tmpy, uint32_t x0, uint32_t y0) {
+    tmpx = x0 = state[0];
+    tmpy = y0 = state[1];
+    for (unsigned int j = 2; j < brans; j += 2) {
+        tmpx ^= state[j];
+        tmpy ^= state[j + 1];
+    }
+    tmpx = ELL(tmpx);
+    tmpy = ELL(tmpy);
+    for (unsigned int j = 2; j < brans; j += 2) {
+        state[j - 2] = state[j + brans] ^ state[j] ^ tmpy;
+        state[j + brans] = state[j];
+        state[j - 1] = state[j + brans + 1] ^ state[j + 1] ^ tmpx;
+        state[j + brans + 1] = state[j + 1];
+    }
+    state[brans - 2] = state[brans] ^ x0 ^ tmpy;
+    state[brans] = x0;
+    state[brans - 1] = state[brans + 1] ^ y0 ^ tmpx;
+    state[brans + 1] = y0;
+}
+
+
+// Proof of concept for using the unrolled linear layer. Only needs to load to registers
+// once, then performs all steps while keeping state in the registers for the linear layer.
+// Only works on 8 brans as it's just a test.
+//
+// This turns out to be much slower than the reference version, as accessing the items in
+// the SIMD registers through the union causes (presumably) a lot of reading/writing from
+// the registers to main memory and back again. This seems to massively outweigh the benefit
+// from not having to load/store at every single step.
+__inline void sparkle_unrolled_test_00(uint32_t* state, int brans, int steps) {
+    if (brans != 8) return;
+
+    uint32_t rc, tmpx, tmpy, x0, y0;
+    
+    struct state_512 simd_state;
+
+    simd_state.vecs[0] = _mm256_i32gather_epi32(state,
+        _mm256_setr_epi32(0, 2, 8, 10, 4, 6, 12, 14), sizeof(uint32_t));
+    simd_state.vecs[1] = _mm256_i32gather_epi32(state,
+        _mm256_setr_epi32(1, 3, 9, 11, 5, 7, 13, 15), sizeof(uint32_t));
+
+    for (unsigned int i = 0; i < steps; i++) {
+        simd_state.arr[INDEX_1] ^= RCON[i % MAX_BRANCHES];
+        simd_state.arr[INDEX_3] ^= i;
+
+        ALZETTE_256(simd_state.vecs[0], simd_state.vecs[1], *rc_256_shuffled);
+
+        linear_layer_unrolled(&(simd_state.arr), brans, tmpx, tmpy, x0, y0);
+    }
+
+    {
+        __m256i temp_j = simd_state.vecs[0];
+        simd_state.vecs[0] = _mm256_unpacklo_epi32(temp_j, simd_state.vecs[1]);
+        simd_state.vecs[1] = _mm256_unpackhi_epi32(temp_j, simd_state.vecs[1]);
+    }
+
+    memcpy(state, &(simd_state.arr), 64);
+}
+
+
+// Similar to previous, but instead of trying to perform the entire linear layer in the
+// SIMD registers, instead write back linearly after Alzette. The idea is that the
+// gather and unpack extra instructions are now only needed in the first and last step
+// respectively, and everywhere else can just use a linear load/store.
+//
+// This does turn out to be faster than reference, but is still approx. 10% behind the
+// rolled version (alzette_avx_03). Unsure why this is slower than that version, as it
+// should in theory be requiring only a single set of unpack instructions per call instead
+// of a set per step. Perhaps the shuffled indices in the linear layer cause more cache
+// misses, or are worse for fetching. This needs more investigation.
+__inline void sparkle_unrolled_test_01(uint32_t* state, int brans, int steps) {
+    if (brans != 8) return;
+
+    uint32_t rc, tmpx, tmpy, x0, y0;
+
+    for (unsigned int i = 0; i < steps; i++) {
+        __m256i state_j, state_j1;
+
+        // Only needs a gathered load for the first step, after that linear is fine.
+        if (i == 0) {
+            // We haven't shuffled the state yet, so use normal indices.
+            state[1] ^= RCON[i % MAX_BRANCHES];
+            state[3] ^= i;
+
+            state_j = _mm256_i32gather_epi32(state,
+                _mm256_setr_epi32(0, 2, 8, 10, 4, 6, 12, 14), sizeof(uint32_t));
+            state_j1 = _mm256_i32gather_epi32(state,
+                _mm256_setr_epi32(1, 3, 9, 11, 5, 7, 13, 15), sizeof(uint32_t));
+        } else {
+            state[INDEX_1] ^= RCON[i % MAX_BRANCHES];
+            state[INDEX_3] ^= i;
+
+            state_j = _mm256_loadu_epi32(state);
+            state_j1 = _mm256_loadu_epi32(state + 8);
+        }
+
+        ALZETTE_256(state_j, state_j1, *rc_256_shuffled);
+
+        // If this is the last step, unpack before storing and use the original indices
+        // for the linear layer. If not, store without unpacking and use the shuffled
+        // indices for the linear layer.
+        if (i == steps - 1) {
+            __m256i state_lo = _mm256_unpacklo_epi32(state_j, state_j1);
+            __m256i state_hi = _mm256_unpackhi_epi32(state_j, state_j1);
+
+            uint32_t* lo_ptr = (uint32_t*)&state_lo;
+            uint32_t* hi_ptr = (uint32_t*)&state_hi;
+            memcpy(state, lo_ptr, 32);
+            memcpy(state + 8, hi_ptr, 32);
+
+            linear_layer_rolled(state, brans, tmpx, tmpy, x0, y0);
+        } else {
+            uint32_t* lo_ptr = (uint32_t*)&state_j;
+            uint32_t* hi_ptr = (uint32_t*)&state_j1;
+            memcpy(state, lo_ptr, 32);
+            memcpy(state + 8, hi_ptr, 32);
+
+            linear_layer_unrolled(state, brans, tmpx, tmpy, x0, y0);
+        }
+    }
+}
